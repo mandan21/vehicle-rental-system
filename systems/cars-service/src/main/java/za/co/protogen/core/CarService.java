39e5134b3ee3ba6292b53495d8821db42077c1c6
Amanda <amanda@pop-os.localdomain>
2024-11-04 21:20:06 +0200

    an interface for the getter and setter methods

--- systems/cars-service/src/main/java/za/co/protogen/core/CarService.java
+++ systems/cars-service/src/main/java/za/co/protogen/core/CarService.java
@@ -1,4 +1,21 @@
 package za.co.protogen.core;
 
-public class CarService {
+import za.co.protogen.domain.Car;
+import java.util.List;
+
+public interface CarService {
+    void addCar(Car car);
+    void removeCar(String vin);
+    Car getCarById(String vin);
+    List<Car> getAllCars();
+    List<Car> getCarsByMake(String make);
+    List<Car> getCarsByYear(int year);
+    List<Car> getCarsByColor(String color);
+    void updateCar(Car car);
+    double calculateAverageMileage();
+    Car findCheapestCar();
+    Car findMostExpensiveCar();
+    Car findNewestCar();
+    Car findOldestCar();
+    List<Car> searchCars(String make, String model, double minPrice, double maxPrice);
 }
