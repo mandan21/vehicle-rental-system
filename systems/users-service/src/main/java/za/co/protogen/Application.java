4067a3ed5189a829ee70d08406b2cde3ed70790b
Amanda <amanda@pop-os.localdomain>
2024-11-06 20:02:28 +0200

    applications main method to run the code

--- systems/users-service/src/main/java/za/co/protogen/Application.java
+++ systems/users-service/src/main/java/za/co/protogen/Application.java
@@ -1,4 +1,28 @@
 package za.co.protogen;
 
+import za.co.protogen.core.UserService;
+import za.co.protogen.core.impl.UserServiceImpl;
+import za.co.protogen.domain.User;
+
+import java.time.LocalDate;
+
 public class Application {
+    public static void main(String[] args) {
+        UserService userService = new UserServiceImpl();
+
+        // Display all users
+        System.out.println("All users: " + userService.getAllUsers());
+
+        // Creating a new user
+        User newUser = new User();
+        newUser.setId(3L); // Ensure unique ID
+        newUser.setFirstName("Alice");
+        newUser.setLastName("Johnson");
+        newUser.setDateOfBirth(LocalDate.of(1992, 3, 10));
+        newUser.setRsaId("1122334455667");
+
+        // Adding the new user
+        userService.addUser(newUser);
+        System.out.println("User added. All users: " + userService.getAllUsers());
+    }
 }
